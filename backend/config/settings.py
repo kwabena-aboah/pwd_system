1006d40691aec085d41707f0fdf2f7da43e06abc
kwabena-aboah <obed30mintah@yahoo.com>
2026-04-25 21:52:19 +0000

    updated settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -169,8 +169,7 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 <<<<<<< HEAD
 OPENAI_API_KEY = config('OPENAI_API_KEY', default='')
 =======
-OPENAI_API_KEY = config('OPENAI_API_KEY', default='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')
->>>>>>> 09e9bd2 (first commit)
+OPENAI_API_KEY = config('OPENAI_API_KEY', default='')
 
 # Auditlog
 AUDITLOG_INCLUDE_ALL_MODELS = False
